b693f77e3f44f0c6f67a4de2f1e15ace882cc582
Manabendra <mkchakraborty5@gmail.com>
2021-02-25 22:40:33 +0600

    pathlib problem

---
+++ basics.py
@@ -0,0 +1,13 @@
+rom pathlib import Path
+path = Path("module_practice\\__init__.py")
+path.exists()
+path.is_file()
+path.is_dir()
+print(path.name)
+print(path.stem)
+print(path.suffix)
+print(path.parent)
+# path = path.with_name("file.txt")
+# print(path.absolute)
+path = path.with_suffix(".txt")
+print(path)
